ee8ad57f50b3b18b67d105fdee348e1943c442a1
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-07 10:41:47 -0300

    Ok

---
+++ ChatServerApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChatServerApp.Comunicacion;
+
+namespace ChatServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = int.Parse(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando servidor en el puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando clientes...");
+                    if (servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Conexion establecida!");
+
+                        //Protocolo de comunicación.
+                        string mensaje = "";
+                        while (mensaje.ToLower() != "chao")
+                        {
+                            //Leer mensaje del cliente.
+                            mensaje = servidor.Leer();
+                            Console.WriteLine("El cliente dice: {0}", mensaje);
+                            if (mensaje.ToLower() != "chao")
+                            {
+                                //El cliente espera una respuesta.
+                                Console.WriteLine("Escribir respuesta...");
+                                mensaje = Console.ReadLine().Trim();
+                                servidor.Escribir("El servidor dice: " + mensaje);
+                            }
+                        }
+                        servidor.CerrarConexion();
+                    }
+                }
+
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error al levantar el servidor");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ClienteSocketApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using ClienteSocketApp.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteSocketApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = ConfigurationManager.AppSettings["ip"];
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ClienteSocket clienteSocket = new ClienteSocket(ip, puerto);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Conectando...");
+            if (clienteSocket.Conectar())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Conectado!");
+                string mensaje = "";
+                while (mensaje.ToLower() != "chao")
+                {
+                    Console.WriteLine("Escribir mensaje...");
+                    mensaje = Console.ReadLine().Trim();
+                    clienteSocket.Escribir(mensaje);
+
+                    if (mensaje.ToLower() != "chao")
+                    {
+                        mensaje = clienteSocket.Leer();
+                        Console.WriteLine(mensaje);
+                    }
+                }
+                clienteSocket.Desconectar();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error de conexión");
+            }
+        }
+    }
+}
